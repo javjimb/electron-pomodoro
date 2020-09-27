45bbadabbfa740699309c6c0b230bdf40d89a4a9
Javier <javjimb@gmail.com>
2020-09-27 10:24:19 +0200

    Add sounds to timer played with Howler

--- app/components/Timer.tsx
+++ app/components/Timer.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import styles from './Timer.css';
+import {Howl} from 'howler';
 
 interface Props {
   sessionLength: number;
@@ -11,6 +12,11 @@ const Timer: React.FC<Props> = ({sessionLength}) => {
   const [secondsLeft, setSecondsLeft] = useState<number>(0);
   const [timer, setTimer] = useState<number | null>(null);
 
+  // timer sounds
+  const soundStart:Howl = new Howl({src: ['../resources/sounds/timer_start.wav'], preload: true});
+  const soundStop:Howl = new Howl({src: ['../resources/sounds/timer_stop.wav'], preload: true});
+  const soundFinish:Howl = new Howl({src: ['../resources/sounds/timer_finish.wav'], preload: true});
+
   const calculateMinutesLeft = (): number => {
     return Math.floor(totalSeconds / 60);
   };
@@ -20,29 +26,44 @@ const Timer: React.FC<Props> = ({sessionLength}) => {
   };
 
   useEffect(() => {
-    if (!totalSeconds) return;
+    if (!totalSeconds) {
+      console.info(totalSeconds);
+      //soundFinish.play();
+      resetTimer();
+      return;
+    };
 
     const intervalId = setInterval(() => {
-        console.log(totalSeconds);
         setTotalSeconds(totalSeconds - 1);
         setSecondsLeft(calculateSecondsLeft());
         setMinutesLeft(calculateMinutesLeft());
+        if (totalSeconds === 1) {
+          //soundFinish.play();
+          //resetTimer();
+        }
     }, 1000);
     setTimer(intervalId);
 
     return () => clearInterval(intervalId);
+
   }, [totalSeconds]);
 
   const startTimer = (): void => {
     setTotalSeconds(sessionLength);
+    soundStart.play();
   };
 
   const stopTimer = (): void => {
+    soundStop.play();
     clearInterval(timer);
+    resetTimer();
+  };
+
+  const resetTimer = (): void => {
     setTotalSeconds(0);
     setSecondsLeft(0);
     setMinutesLeft(0);
-  };
+  }
 
   const padNum = (num: number): string => {
     return (num < 10 ? '0' : '') + num.toString();
