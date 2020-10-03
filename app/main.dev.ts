a108e697e7d3673077db55f341fad19df7f7ab83
Javier <javjimb@gmail.com>
2020-10-03 12:43:06 +0200

    Adjust browser window size

--- app/main.dev.ts
+++ app/main.dev.ts
@@ -58,8 +58,8 @@ const createWindow = async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728,
+    width: 275,
+    height: 425,
     webPreferences:
       (process.env.NODE_ENV === 'development' ||
         process.env.E2E_BUILD === 'true') &&
